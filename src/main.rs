c9bc45e2b696b4c49bf75298503f850584aa0e71
Lennon Donaghy <donaghylennon@gmail.com>
2022-02-08 21:24:31 +0000

    Add initial type representation of markdown

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+enum MarkdownText {
+    Text(String),
+    Bold(Box<MarkdownText>),
+    Italic(Box<MarkdownText>),
+}
+
+enum Markdown {
+    Heading(u8, Box<MarkdownText>),
+    Paragraph(Box<MarkdownText>),
+    BlockQuote(Box<MarkdownText>),
+    List(ListType, Vec<MarkdownText>),
+    Code(String),
+    HorizontalRule,
+}
+
+enum ListType {
+    Ordered, Unordered
+}
+
+type MarkdownFile = Vec<Markdown>;
+
 fn main() {
     println!("Hello, world!");
 }

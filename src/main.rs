1b4410ad1c3575ec8e51052e5c8da864919c8010
Lennon Donaghy <donaghylennon@gmail.com>
2022-02-13 02:42:37 +0000

    Add parsing functions for text, including bold and italic
    Only works in headings for now, and if a * or _ are unmatched by the end
    of a line it is just treated as closed by the end of the line.

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,13 @@ type MarkdownFile = Vec<Markdown>;
 
 fn main() {
     if let Some(md_file) = parse_md_from_file("examples/heading.md") {
-        println!("File contains:\n{:?}", md_file);
+        println!("heading.md contains:\n{:?}", md_file);
+    } else {
+        println!("Something went wrong!");
+    }
+
+    if let Some(md_file) = parse_md_from_file("examples/bold_italic.md") {
+        println!("bold_italic.md contains:\n{:?}", md_file);
     } else {
         println!("Something went wrong!");
     }
@@ -72,9 +78,81 @@ fn parse_heading(chars: &mut Peekable<Chars>) -> Markdown {
         chars.next();
         num += 1;
     }
-    let text = chars
+    let text: String = chars
         .skip_while(|&c| c.is_whitespace())
         .take_while(|&c| c != '\n')
         .collect();
-    Markdown::Heading(num, vec![MarkdownText::Text(text)])
+    Markdown::Heading(num, parse_md_text(&mut text.chars().peekable()))
+}
+
+fn parse_md_text(chars: &mut Peekable<Chars>) -> Vec<MarkdownText> {
+    let mut md_text = vec![];
+    while let Some(&ch) = chars.peek() {
+        match ch {
+            '*' => {
+                chars.next();
+                md_text.push(parse_md_text_bold(chars));
+            }
+            '_' => {
+                chars.next();
+                md_text.push(parse_md_text_italic(chars));
+            }
+            _   => {
+                md_text.push(parse_md_text_text(chars));
+            }
+        }
+    }
+    md_text
+}
+
+fn parse_md_text_bold(chars: &mut Peekable<Chars>) -> MarkdownText {
+    let mut md_text = vec![];
+    while let Some(&ch) = chars.peek() {
+        match ch {
+            '*' => {
+                chars.next();
+                return MarkdownText::Bold(md_text);
+            }
+            '_' => {
+                chars.next();
+                md_text.push(parse_md_text_italic(chars));
+            }
+            '\n' => {
+                chars.next();
+                return MarkdownText::Bold(md_text);
+            }
+            _   => {
+                md_text.push(parse_md_text_text(chars));
+            }
+        }
+    }
+    MarkdownText::Bold(md_text)
+}
+
+fn parse_md_text_italic(chars: &mut Peekable<Chars>) -> MarkdownText {
+    let mut md_text = vec![];
+    while let Some(&ch) = chars.peek() {
+        match ch {
+            '*' => {
+                chars.next();
+                md_text.push(parse_md_text_bold(chars));
+            }
+            '_' => {
+                chars.next();
+                return MarkdownText::Italic(md_text);
+            }
+            '\n' => {
+                chars.next();
+                return MarkdownText::Italic(md_text);
+            }
+            _   => {
+                md_text.push(parse_md_text_text(chars));
+            }
+        }
+    }
+    MarkdownText::Italic(md_text)
+}
+
+fn parse_md_text_text(chars: &mut Peekable<Chars>) -> MarkdownText {
+    MarkdownText::Text(chars.take_while(|&c| c != '*' && c != '_').collect())
 }
